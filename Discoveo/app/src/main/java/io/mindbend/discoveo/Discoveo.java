8c139ae823c76dd706e68d8f9d420467d6d6bc9e
Akshay Pall <iosakshay@gmail.com>
2015-08-15 12:12:56 -0400

    Finished Discovers & Results Adapter

--- Discoveo/app/src/main/java/io/mindbend/discoveo/Discoveo.java
+++ Discoveo/app/src/main/java/io/mindbend/discoveo/Discoveo.java
@@ -4,7 +4,26 @@ package io.mindbend.discoveo;
  * Created by rohitsharma on 2015-08-15.
  */
 public class Discoveo {
-    public Discoveo(){
-        //TODO: here
+
+    private String mTitle;
+    private String mDetail;
+    private double mRatingDouble;
+
+    public Discoveo(String title, String detail, double rating){
+        mTitle = title;
+        mDetail = detail;
+        mRatingDouble = rating;
+    }
+
+    public String getTitle(){
+        return mTitle;
+    }
+
+    public String getDetail() {
+        return mDetail;
+    }
+
+    public String getRatingString(){
+        return ""+mRatingDouble;
     }
 }
--- Discoveo/app/src/main/java/io/mindbend/discoveo/ResultsListAdapter.java
+++ Discoveo/app/src/main/java/io/mindbend/discoveo/ResultsListAdapter.java
@@ -11,98 +11,77 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * Created by rohitsharma on 2015-08-15.
+ * Created by akshaypall on 2015-08-15.
  */
-public class ResultsListAdapter {
-    package io.mindbend.android.announcements.reusableFrags;
 
-    import android.content.Context;
-    import android.content.Intent;
-    import android.os.Bundle;
-    import android.support.v4.app.Fragment;
-    import android.support.v4.app.FragmentActivity;
-    import android.support.v4.app.FragmentTransaction;
-    import android.support.v7.widget.RecyclerView;
-    import android.util.Log;
-    import android.view.LayoutInflater;
-    import android.view.View;
-    import android.view.ViewGroup;
-    import android.widget.TextView;
+/**
+ * Created by Akshay Pall on 01/08/2015.
+ */
+public class ResultsListAdapter extends RecyclerView.Adapter<ResultsListAdapter.ViewHolder> implements Serializable {
 
-    import java.io.Serializable;
-    import java.util.List;
+    private static final String TAG = "ResultsListAdapter";
 
-    import io.mindbend.android.announcements.R;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView mTitle;
+        private final TextView mDetail;
+        private final TextView mRatingText;
+        private final de.hdodenhof.circleimageview.CircleImageView mImage;
 
-    /**
-     * Created by Akshay Pall on 01/08/2015.
-     */
-    public class ResultsListAdapter extends RecyclerView.Adapter<ResultsListAdapter.ViewHolder> implements Serializable {
+        //TODO: create private fields for the elements within a single feed item
 
-        private static final String TAG = "ResultsListAdapter";
+        public ViewHolder(View itemView) {
+            super(itemView);
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            private final TextView mTitle;
-            private final TextView mDetail;
-            private final TextView mRatingText;
-            private final de.hdodenhof.circleimageview.CircleImageView mImage;
-
-            //TODO: create private fields for the elements within a single feed item
-
-            public ViewHolder(View itemView) {
-                super(itemView);
-
-                //getting all the elements part of the card, aside from the image
-                mTitle = (TextView) itemView.findViewById(R.id.discoveo_title);
-                mDetail = (TextView) itemView.findViewById(R.id.discoveo_description);
-                mRatingText = (TextView) itemView.findViewById(R.id.discoveo_rating_string);
-                mImage = (de.hdodenhof.circleimageview.CircleImageView) itemView.findViewById(R.id.discoveo_image);
-            }
-        }
-
-        //TODO: create private fields for the list
-        private List<Discoveo> mDiscoveos;
-        private Context mContext;
-
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.result_discoveo_feed_item, viewGroup, false);
-            return new ViewHolder(v);
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, int i) {
-            final Discoveo discoveo = mDiscoveos.get(i);
-
-            //TODO: setup view
-            viewHolder.mTitle.setText(discoveo.getTitle());
-            viewHolder.mDetail.setText(discoveo.getDetail());
-            viewHolder.mRating.setText(discoveo.getRatingText());
-
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mDiscoveos.size();
-        }
-
-        public ResultsListAdapter (Context context, List<Discoveo> discoveos){
-            //save the mPosts private field as what is passed in
-            mContext = context;
-            mDiscoveos = discoveos;
-        }
-
-        @Override
-        public void onViewAttachedToWindow(ViewHolder holder) {
-            super.onViewAttachedToWindow(holder);
-        }
-
-        @Override
-        public void onViewDetachedFromWindow(ViewHolder holder) {
-            super.onViewDetachedFromWindow(holder);
+            //getting all the elements part of the card, aside from the image
+            mTitle = (TextView) itemView.findViewById(R.id.discoveo_title);
+            mDetail = (TextView) itemView.findViewById(R.id.discoveo_description);
+            mRatingText = (TextView) itemView.findViewById(R.id.discoveo_rating_string);
+            mImage = (de.hdodenhof.circleimageview.CircleImageView) itemView.findViewById(R.id.discoveo_image);
         }
     }
 
+    //TODO: create private fields for the list
+    private List<Discoveo> mDiscoveos;
+    private Context mContext;
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.result_discoveo_feed_item, viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        final Discoveo discoveo = mDiscoveos.get(i);
+
+        //TODO: setup view
+        viewHolder.mTitle.setText(discoveo.getTitle());
+        viewHolder.mDetail.setText(discoveo.getDetail());
+        viewHolder.mRatingText.setText(discoveo.getRatingString());
+        viewHolder.mImage.setImageResource(R.drawable.outside_of_student_design_centre);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDiscoveos.size();
+    }
+
+    public ResultsListAdapter(Context context, List<Discoveo> discoveos) {
+        //save the mPosts private field as what is passed in
+        mContext = context;
+        mDiscoveos = discoveos;
+    }
+
+    @Override
+    public void onViewAttachedToWindow(ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+    }
+
+    @Override
+    public void onViewDetachedFromWindow(ViewHolder holder) {
+        super.onViewDetachedFromWindow(holder);
+    }
 }
+
